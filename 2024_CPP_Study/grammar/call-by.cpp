a8f674740db014376f42883e9cac99fb5e2ec6cc
Najisu <s2322@e-mirim.hs.kr>
2024-03-26 15:32:42 +0900

    call by value

---
+++ 2024_CPP_Study/grammar/call-by.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void call_by_value(int val)
+{
+	// 새로운 지역변수 val을 만들고 값을 넣는다.
+	val = 10;
+}
+
+int main(void)
+{
+	int num = 4;
+	// num의 값은 변하지 않는다.
+	call_by_value(num);
+	cout << num;
+	return 0;
+}
