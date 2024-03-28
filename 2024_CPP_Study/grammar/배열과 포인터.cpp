cefd0c989bcadbb8aaf2bb796f4f0be45b4dd0be
Najisu <s2322@e-mirim.hs.kr>
2024-03-28 11:08:00 +0900

    포인터 연산

--- 2024_CPP_Study/grammar/배열과 포인터.cpp
+++ 2024_CPP_Study/grammar/배열과 포인터.cpp
@@ -12,6 +12,18 @@ int main(void)
 	// 주소값 1 바이트 만큼의 차이
 	printf("%p %p \n", *(arr+0), *(arr+1));
 
+	int i = 1;
+	char c = 'b';
+	double d = 3.0;
+
+	int* pi = &i;
+	char* pc = &c;
+	double* pd = &d;
+
+	// 각각 4, 1, 8 차이
+	printf("%d %d %d \n", pi, pc, pd);
+	printf("%d %d %d \n", pi + 1, pc + 1, pd + 1);
+
 	return 0;
 
 }
