432fe38b92a825615e535beda1613a3b21b2ab8c
Najisu <s2322@e-mirim.hs.kr>
2024-04-02 15:46:28 +0900

    call by reference

--- 2024_CPP_Study/grammar/call-by.cpp
+++ 2024_CPP_Study/grammar/call-by.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-void call_by_pointer(int* val)
+void call_by_reference(int& val)
 {
 	// 새로운 지역변수 val을 만들고 값을 넣는다.
-	*val = 10;
+	val = 10;
 }
 
 int main(void)
 {
 	int num = 4;
 	// num의 값은 바뀐다.
-	call_by_pointer(&num);
+	call_by_reference(num);
 	cout << num;
 	return 0;
 }
