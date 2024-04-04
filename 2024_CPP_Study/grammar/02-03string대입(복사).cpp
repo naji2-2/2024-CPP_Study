08240c8c21ab1f5996e5a6ef62a57eb1ad372604
Najisu <s2322@e-mirim.hs.kr>
2024-04-04 11:48:05 +0900

    string대입(cpp방식)

--- 2024_CPP_Study/grammar/02-03string대입(복사).cpp
+++ 2024_CPP_Study/grammar/02-03string대입(복사).cpp
@@ -1,13 +1,14 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>
 
 int main(void)
 {
-	char str[30] = "develop Jisu";
-	char str2[30];
+	std::string str = "develop Jisu";
+	std::string str2;
 
 	// str에 있는 내용을 str2에 복사
-	strcpy(str2, str);
-	printf("%s", str2);
+	str2 = str;
+
+	std::cout << str2;
 
 }
