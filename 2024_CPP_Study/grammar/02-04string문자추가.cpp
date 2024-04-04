389ea5390f55dbfda13f9ee4687f942dcd0f14e0
Najisu <s2322@e-mirim.hs.kr>
2024-04-04 11:56:54 +0900

    string문자열추가(cpp방식)

--- 2024_CPP_Study/grammar/02-04string문자추가.cpp
+++ 2024_CPP_Study/grammar/02-04string문자추가.cpp
@@ -1,11 +1,15 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>
+
+using namespace std;;
+
 
 int main(void)
 {
-	char str[30] = "c";
-	strcat(str, "addition");
+	string str = "c++";
+	//기존 문자열에 추가
+	str += "addition";
 
-	printf("%s", str);
+	cout << str;
 
 }
