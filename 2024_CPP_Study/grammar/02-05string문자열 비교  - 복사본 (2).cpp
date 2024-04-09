0ecbd08c301b151358bd274460f930ede5cf5483
Najisu <s2322@e-mirim.hs.kr>
2024-04-09 15:41:49 +0900

    구조체는 default가 public이다

---
+++ 2024_CPP_Study/grammar/02-05string문자열 비교  - 복사본 (2).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string str = "c++";
+	//기존 문자열에 추가
+	if (str == "c")
+		cout << "같은 문자열";
+	else
+		cout << "다른 문자열";
+}
---
+++ 2024_CPP_Study/grammar/03-01 구조체와 클래스.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student {
+	int hakbun_;
+	string name_;
+};
+
+int main(void)
+{
+	struct Student jisu;
+	// 구조체는 default가 public이다
+	jisu.hakbun_ = 2202;
+	jisu.name_ = "나지수";
+
+	return 0;
+}
