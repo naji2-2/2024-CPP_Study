0374770f32c569850d4e9285e7226e6212cc3293
Najisu <s2322@e-mirim.hs.kr>
2024-04-11 11:06:03 +0900

    03-01 멤버함수 만들기

--- 2024_CPP_Study/grammar/03-01 구조체와 클래스.cpp
+++ 2024_CPP_Study/grammar/03-01 구조체와 클래스.cpp
@@ -7,14 +7,23 @@ class Student {
 public:
 	// 생성자 : 객체가 생성될 때 호출되는 함수
 	// 생성자는 반환형이 없다.
-	Student() {
+	Student()
+	{
 		hakbun_ = 2222;
 		name_ = "mirim";
 	}
 
-	Student(int hakbun, string name) {
+	Student(int hakbun, string name)
+	{
 		hakbun_ = hakbun;
 		name_ = name;
+	}
+
+	// 클래스의 멤버를 출력
+	void show(void)
+	{
+		cout << hakbun_ << " ";
+		cout << name_ << endl;
 	}
 
 private: 
@@ -26,6 +35,9 @@ int main(void)
 {
 	Student jisu = Student(2202, "나지수");
 	Student mirim = Student();
+
+	jisu.show();
+	mirim.show();
 
 	return 0;
 }
