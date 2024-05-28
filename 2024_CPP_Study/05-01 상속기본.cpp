20ea92fc809d180fc6c600b0639dd65ffca317f0
naji2-2 <s2322@e-mirim.hs.kr>
2024-05-28 16:06:04 +0900

    05-01 부모, 자식 / 생성, 소멸자 호출순서

--- 2024_CPP_Study/05-01 상속기본.cpp
+++ 2024_CPP_Study/05-01 상속기본.cpp
@@ -6,7 +6,14 @@ using namespace std;
 class Person {
 public:
 	Person(string name, int age)
-		: name_(name), age_(age){}
+		: name_(name), age_(age)
+	{
+		cout << "부모 생성자 호출" << endl;
+	}
+
+	~Person() {
+		cout << "부모 소멸자 호출" << endl;
+	}
 
 	void show_person() {
 		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
@@ -23,8 +30,13 @@ public:
 	// 부모 생성자가 먼저 호출
 	Student(string name, int age, int id) : Person(name, age), id_(id)
 	{
-
+		cout << "자식 생성자 호출" << endl;
 	}
+
+	~Student() {
+		cout << "자식 소멸자 호출" << endl;
+	}
+
 	void show_person()
 	{
 		cout << id_ << " ";
@@ -38,5 +50,7 @@ private:
 int main(void)
 {
 	Student* stu = new Student("나지수", 17, 2202);
+	stu->show_person();
+	delete stu;
 	return 0;
 }
