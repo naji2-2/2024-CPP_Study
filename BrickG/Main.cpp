7d82778934ed70687a4fb59bbaade63e5084be35
naji2-2 <s2322@e-mirim.hs.kr>
2024-06-25 15:52:38 +0900

    배경을 하얀색으로

--- BrickG/Main.cpp
+++ BrickG/Main.cpp
@@ -3,12 +3,12 @@
 int main()
 {
     // 창 생성
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Shapes");
-    window.setFramerateLimit(60);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "bricks");
+    window.setFramerateLimit(60);   // 초당 프레임을 60으로
 
     // 원 객체 생성
     sf::CircleShape circle(100.f);      // 반지름
-    circle.setFillColor(sf::Color::Red);
+    circle.setFillColor(sf::Color::Magenta);
     circle.setPosition(0.f, 0.f);       // 위치
 
     // 사각형 객체 생성
@@ -19,16 +19,17 @@ int main()
     // 이벤트 루프 시작
     while (window.isOpen())
     {
+        rectangle.move(0.5f, 0);
         sf::Event event;
         while (window.pollEvent(event))
         {
-            rectangle.move(0.5f, 0);
+            // x 마크를 누르면 윈도우창을 끈다
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         // 화면 지우기
-        window.clear();
+        window.clear(sf::Color::White);
 
         // 도형 그리기
         window.draw(circle);
