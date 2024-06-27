072cadd008200fdfb1d528d7e339404fcb86f859
naji2-2 <s2322@e-mirim.hs.kr>
2024-06-27 12:00:58 +0900

    paddle 추가, 공 구현

--- BrickG/Main.cpp
+++ BrickG/Main.cpp
@@ -1,10 +1,75 @@
 #include <SFML/Graphics.hpp>
 
+class Ball {
+public:
+    sf::CircleShape shape;
+    sf::Vector2f velocity;
+
+    Ball(float mX, float mY) {
+        shape.setPosition(mX, mY);
+        shape.setRadius(10.f);
+        shape.setFillColor(sf::Color::Red);
+        shape.setOrigin(10.f, 10.f);    //기준점이 원점(정중앙)으로 바뀜
+        velocity = { -8.f, -8.f };      //한 프레임 당 왼쪽으로 8픽셀씩, 위로 8픽셀씩 이동
+    }
+
+    void update() {
+        shape.move(velocity);
+
+        if (left() < 0) velocity.x = 8.f;
+        else if (right() > 800) velocity.x = -8.f;
+
+        if (top() < 0) velocity.y = 8.f;
+        else if (bottom() > 600) velocity.y = -8.f;
+    }
+
+    float left() { return shape.getPosition().x - shape.getRadius(); }
+    float right() { return shape.getPosition().x + shape.getRadius(); }
+    float top() { return shape.getPosition().y - shape.getRadius(); }
+    float bottom() { return shape.getPosition().y + shape.getRadius(); }
+};
+
+// Paddle 클래스 정의
+class Paddle {
+public:
+    sf::RectangleShape shape;
+    const float paddleWidth = 100.f;
+    const float paddleHeight = 20.f;
+    const float paddleVelocity = 10.f;
+
+    Paddle(float mX, float mY) {
+        shape.setPosition(mX, mY);
+        shape.setSize({ paddleWidth, paddleHeight });
+        shape.setFillColor(sf::Color::Blue);
+        // 기준점을 중심으로
+        shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
+    }
+
+    void update() {
+        // 왼쪽 화살키는 누르고 왼쪽 끝에 도달하지 않은 경우
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && left() > 0) {
+            shape.move(-paddleVelocity, 0.f);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && right() < 800) {
+            shape.move(paddleVelocity, 0.f);
+        }
+    }
+
+    float left() { return shape.getPosition().x - shape.getSize().x / 2.f; }
+    float right() { return shape.getPosition().x + shape.getSize().x / 2.f; }
+    // 지금 당장은 쓸모가 없지만, 확장을 고려해서 일단 놔둠.
+    float top() { return shape.getPosition().y - shape.getSize().y / 2.f; }
+    float bottom() { return shape.getPosition().y + shape.getSize().y / 2.f; }
+};
+
 int main()
 {
     // 창 생성
     sf::RenderWindow window(sf::VideoMode(800, 600), "bricks");
     window.setFramerateLimit(60);   // 초당 프레임을 60으로
+
+    Ball ball(800 / 2.f, 300.f);
+    Paddle paddle(600.f, 550.f);
 
 
     // 이벤트 루프 시작
@@ -18,11 +83,17 @@ int main()
                 window.close();
         }
 
+        // update
+        paddle.update();
+        ball.update();
+
+        // draw
         // 화면 지우기
         window.clear(sf::Color::White);
 
         // 그리기
-     
+        window.draw(paddle.shape);
+        window.draw(ball.shape);
 
         // 화면 업데이트
         window.display();
