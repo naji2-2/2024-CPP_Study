2099a407eb9a14486a810c8d85f3c41ec61ca9f1
naji2-2 <s2322@e-mirim.hs.kr>
2024-08-29 22:42:21 +0900

    다형성 복습

---
+++ 2024_CPP_Study/다형성 예제.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FoodKingdom {
+public:
+	FoodKingdom(int civil, int force, string name, int territory)
+		: civil_(civil), force_(force), name_(name), territory_(territory)
+	{
+
+	}
+private:
+	int civil_; // ±ππŒ ºˆ
+	int force_; // ±∫∑¬
+	string name_; // ¿Ã∏ß
+	int territory_; // ∂•(∏È¿˚)
+};
+
+class Kimchi : public FoodKingdom{
+public:
+	Kimchi(int civil, int force, string name, int territory, int garlic, int pepper) 
+		: FoodKingdom(civil, force, name, territory), garlic_(garlic), pepper_(pepper)
+	{
+
+	}
+private:
+	int garlic_; // ∏∂¥√
+	int pepper_; // ∞Ì√ﬂ
+};
+
+class Jelly : public FoodKingdom {
+public:
+	Jelly(int civil, int force, string name, int territory, int gelatin, int sugar)
+		: FoodKingdom(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar)
+	{
+
+	}
+private:
+	int gelatin_; // ¡©∂Û∆æ
+	int sugar_; // º≥≈¡¿« »˚
+};
+
+class Cheese : public FoodKingdom {
+public:
+	Cheese(int civil, int force, string name, int territory, int milk, int rennet)
+		: FoodKingdom(civil, force, name, territory), milk_(milk), rennet_(rennet)
+	{
+
+	}
+private:
+	int milk_; // ¡©∂Û∆æ
+	int rennet_; // ¿¿∞Ì¡¶¿« »˚
+};
+
