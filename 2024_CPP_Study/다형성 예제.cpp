67921abfaea4e7fd1f55a7d536ea17dfc6d53560
naji2-2 <s2322@e-mirim.hs.kr>
2024-09-03 15:54:10 +0900

    Reapply "프런트창"
    This reverts commit 29c3f7ffdf52c2fd13f01758977d32ae8989e038.

--- 2024_CPP_Study/다형성 예제.cpp
+++ 2024_CPP_Study/다형성 예제.cpp
@@ -7,8 +7,13 @@ class FoodKingdom {
 public:
 	FoodKingdom(int civil, int force, string name, int territory)
 		: civil_(civil), force_(force), name_(name), territory_(territory)
-	{
+	{}
 
+	virtual void show() {
+		cout << "이름 : " << name_ << endl;
+		cout << "국민 수 : " << civil_ << endl;
+		cout << "군력 : " << force_ << endl;
+		cout << "땅 면적 : " << territory_ << endl;
 	}
 private:
 	int civil_; // 국민 수
@@ -21,8 +26,11 @@ class Kimchi : public FoodKingdom{
 public:
 	Kimchi(int civil, int force, string name, int territory, int garlic, int pepper) 
 		: FoodKingdom(civil, force, name, territory), garlic_(garlic), pepper_(pepper)
-	{
-
+	{}
+	void show() {
+		FoodKingdom::show();
+		cout << "마늘 : " << garlic_ << endl;
+		cout << "고추 : " << pepper_ << endl;
 	}
 private:
 	int garlic_; // 마늘
@@ -33,8 +41,11 @@ class Jelly : public FoodKingdom {
 public:
 	Jelly(int civil, int force, string name, int territory, int gelatin, int sugar)
 		: FoodKingdom(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar)
-	{
-
+	{}
+	void show() {
+		FoodKingdom::show();
+		cout << "젤라틴 : " << gelatin_ << endl;
+		cout << "설탕 : " << sugar_ << endl;
 	}
 private:
 	int gelatin_; // 젤라틴
@@ -45,11 +56,68 @@ class Cheese : public FoodKingdom {
 public:
 	Cheese(int civil, int force, string name, int territory, int milk, int rennet)
 		: FoodKingdom(civil, force, name, territory), milk_(milk), rennet_(rennet)
-	{
-
+	{}
+	void show() {
+		FoodKingdom::show();
+		cout << "우유 : " << milk_ << endl;
+		cout << "응고제 : " << rennet_ << endl;
 	}
 private:
-	int milk_; // 젤라틴
+	int milk_; // 우유
 	int rennet_; // 응고제의 힘
 };
 
+int main(void) 
+{
+	FoodKingdom* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
+	FoodKingdom* friends = new Cheese(100, 20, "짜계치", 100, 20, 100);
+
+	while (true) {
+		system("cls");
+		cout << "★나★" << endl;
+		player->show();
+
+		cout << endl << endl;
+
+		cout << "★상대방★" << endl;
+		friends->show();
+
+		cout << endl << endl;
+		cout << "------------------------------------" << endl;
+
+		int select;
+		cout << "1. 공격" << endl;
+		cout << "2. 특수1" << endl;
+		cout << "3. 특수2" << endl;
+		cout << "4. 방어" << endl;
+
+		cin >> select;
+		switch (select)
+		{
+
+			case 1:
+				//TODO : 공격
+				cout << "공격" << endl;
+				break;
+			case 2:
+				//TODO : 특수 1
+				cout << "특수 1" << endl;
+				break;
+			case 3:
+				//TODO : 특수 2
+				cout << "특수 2" << endl;
+				break;
+			case 4:
+				//TODO : 방어
+				cout << "방어" << endl;
+				break;
+			default:
+				cout << "디폴트" << endl;
+		}
+		system("pause");
+	}
+	
+	delete friends;
+	delete player;
+	return 0;
+}
