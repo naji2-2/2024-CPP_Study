0c1edc52a90b3c0c98c8803e89705287f48c92c6
Najisu <s2322@e-mirim.hs.kr>
2024-09-24 15:48:58 +0900

    08-02 맵

---
+++ 2024_CPP_Study/08-02 맵.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void main(void)
+{
+	map<string, long long> money;
+
+	// ¸Ê Ãß°¡
+	money["Áö¼ö"] = 60000;
+	money["¼öºó"] = 300;
+	money["¹Î¼±"] = 1000;
+
+	// ¹Ýº¹ÀÚ
+	map<string, long long>::iterator iter;
+	for (iter = money.begin(); iter != money.end(); iter++)
+		cout << iter->first << "´Â " << iter->second << endl;
+
+	// °ª ¼öÁ¤
+	money["¹Î¼±"] = 100000;
+	cout << money["¹Î¼±"] << endl;
+
+	// Å©±â
+	cout << money.size() << endl;
+
+}
