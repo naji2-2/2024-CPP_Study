c66bca9ee6849c0e87beedec7a633f411e778d70
Najisu <s2322@e-mirim.hs.kr>
2024-09-26 11:12:53 +0900

    09-01 enum 적용 후

--- 2024_CPP_Study/09-01 enum.cpp
+++ 2024_CPP_Study/09-01 enum.cpp
@@ -2,20 +2,26 @@
 
 using namespace std;
 
+enum FoodStyle {
+	KOREAN,		// 0 -> 0은 디폴트 값, 값을 주고 싶으면 KOREAN = 0으로 한다.
+	JAPANESE,	// 1
+	CHINESE		// 2
+};
+
 void main(void)
 {
-	// 0 : 한식, 1 : 일식, 2 : 중식 
-	int style = 0;
+	FoodStyle style = KOREAN;
 
 	switch (style)
 	{
-		case 0 :
+		// 열거형을 사용하면 가독성도 좋아지고, 중간에 값을 유연하게 추가할 수 있다.
+		case KOREAN:
 			cout << "김치, 쌈, 비빔밥" << endl;
 			break;
-		case 1:
+		case JAPANESE:
 			cout << "초밥, 우동, 타코야끼" << endl;
 			break;
-		case 2:
+		case CHINESE:
 			cout << "동파육, 마라탕, 탕후루" << endl;
 			break;
 		default:
