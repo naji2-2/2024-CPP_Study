a0d8753d9ed5fe4b920ce998fd8605a53a39163f
Najisu <s2322@e-mirim.hs.kr>
2024-10-15 14:20:25 +0900

    09-02 전통적인 반복문

---
+++ 2024_CPP_Study/09-02 범위기반 for문.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main(void)
+{
+
+	// 전통적인 for문
+	vector<int> v = { 445, 14, 27, 456, 23 };
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
+}
