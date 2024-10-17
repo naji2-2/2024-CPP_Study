e821da6bddd75490504a4f2476ab889f617a7f3e
Najisu <s2322@e-mirim.hs.kr>
2024-10-17 12:03:04 +0900

    [ShootingG] const를 이용한 상수처리 방법

--- ShootingG/main.cpp
+++ ShootingG/main.cpp
@@ -3,8 +3,8 @@
 using namespace sf;
 using namespace std;
 
-#define WINDOW_WHDTH	1200
-#define WINDOW_HEIGHT	700
+const int WINDOW_WHDTH = 1200;
+const int WINDOW_HEIGHT = 700;
 
 void main(void)
 {
