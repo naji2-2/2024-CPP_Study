a100890bd0715758e8087a4d035f569d7e20128b
Najisu <s2322@e-mirim.hs.kr>
2024-10-22 14:42:13 +0900

    [ShootingG] 이동하기 구현

--- ShootingG/main.cpp
+++ ShootingG/main.cpp
@@ -12,6 +12,11 @@ void main(void)
 {
 	RenderWindow window(VideoMode(App::WHDTH, App::HEIGHT), "ShootingG");
 
+	RectangleShape player;
+	player.setSize(Vector2f(165.f, 62.f));
+	player.setFillColor(Color::Cyan);
+	player.setPosition((App::WHDTH / 2) - 82.f, 600.f);
+
 	while (window.isOpen()) {
 		Event event;
 		while(window.pollEvent(event))
@@ -20,10 +25,15 @@ void main(void)
 				window.close();
 		}
 
-		RectangleShape player;
-		player.setSize(Vector2f(165.f, 62.f));
-		player.setFillColor(Color::Cyan);
-		player.setPosition(520.f, 360.f);
+		// TODO : 제대로 이동하도록 고치기
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+			player.move(-10,0);
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+			player.move(10, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+			player.move(0, -10);
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+			player.move(0, 10);
 
 		window.clear();
 		window.draw(player);
